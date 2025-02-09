85f6f264939e80d2fce83b788668504b7f7268ea
ValeriiaZinoveva <leraiztlt@gmail.com>
2025-02-10 00:26:58 +0400

    feat: add routing

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ErrorBoundary from './components/ErrorBoundary';
 import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <ErrorBoundary>
-      <Home />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </ErrorBoundary>
   );
 }
